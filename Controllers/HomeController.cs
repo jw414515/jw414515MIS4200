bcfa16471137aa9a3aa2dcd578c4ebe9fdc82e60
Jacob Worm <jw414515@ohio.edu>
2020-01-24 11:48:28 -0500

    New Save

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace jw414515MIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 description.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Feel free to contact me!";
 
             return View();
         }
